f23696d438d58fcf9f69b68457c594142f3ac74b
Yuki <adohiito@gmail.com>
2020-05-25 18:51:58 -0400

    express set up done

--- example-node.js
+++ example-node.js
@@ -1,7 +1,27 @@
 const express = require("express")
-
 const app = express();
 
+const https = require("https")
+
+app.get("/",(req,res)=>{
+//    res.send("Hello World, Hello Yuki")
+    https.get("https://insult.mattbas.org/api/insult",resp=>{
+        resp.on("data", chunk => {
+            res.send(`
+            <http>
+            <head><title>Oh Snap</title></head>
+            <body>
+                <h1>OH SNAP!</h1>
+                <p style="font-size: 24px; color: red;">
+                    ${chunk.toString()}!
+                </p>
+            </body>
+            </http>
+            `);
+        })
+    })
+
+})
 
 
 app.listen(3000)
